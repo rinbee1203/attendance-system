ca6d0f4d35a95e9e7b528a005b1f3a59a10b9386
Rin <aporia1203@gmail.com>
2026-02-25 12:24:27 +0800

    Initial commit

--- server/server.js
+++ server/server.js
@@ -10,10 +10,11 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.use(express.json());
+app.use(express.json({ limit: "2mb" }));
+app.use(express.urlencoded({ limit: "2mb", extended: true }));
 
 // Routes
 app.use("/api/auth", require("./routes/auth"));
