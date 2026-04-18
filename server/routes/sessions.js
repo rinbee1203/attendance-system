3840fadce0669b955e17959f50d339649c6cd589
rinbee1203 <aporia1203@gmail.com>
2026-04-18 10:24:13 +0800

    Add getStudentsForRoster route to sessions

--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const { protect, restrictTo } = require("../middleware/auth");
-const { createSession, startSession, refreshQR, stopSession, getSessions, getSession, deleteSession, updateSession } = require("../controllers/sessionsController");
+const { createSession, startSession, refreshQR, stopSession, getSessions, getSession, deleteSession, updateSession, getStudentsForRoster } = require("../controllers/sessionsController");
 
 // All routes require authentication and teacher role
 router.use(protect, restrictTo("teacher"));
 
 router.get("/", getSessions);
 router.post("/", createSession);
+router.get("/students", getStudentsForRoster); // For roster builder
 router.get("/:id", getSession);
 router.post("/:id/start", startSession);
 router.post("/:id/refresh-qr", refreshQR);
 router.post("/:id/stop", stopSession);
+
 router.patch("/:id", updateSession);
+router.delete("/:id", deleteSession);
 
 module.exports = router;
